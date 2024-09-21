b11590a4220260a324da50fc6dc882cc16b01953
asadbek kholdarov <78337069+asadbekkholdarov@users.noreply.github.com>
2024-09-21 21:23:39 +0500

    Add files via upload

---
+++ sqlexploration.sql
@@ -0,0 +1,86 @@
+Select *
+From SQLPortfolioProject..CovidDeath
+Where continent is not null 
+order by 3,4
+
+
+-- Select Data that we are going to be starting with
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From SQLPortfolioProject..CovidDeath
+Where continent is not null 
+order by 1,2
+
+
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
+
+SELECT 
+    Location, Date, 
+    total_cases, total_deaths, 
+    CASE 
+        WHEN total_cases = 0 THEN 0
+        ELSE (total_deaths / total_cases) * 100 
+    END AS DeathPercentage
+FROM 
+    SQLPortfolioProject..CovidDeath
+WHERE 
+    Location LIKE '%states%' 
+    AND Continent IS NOT NULL
+ORDER BY 
+    Location, Date;
+
+
+
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From SQLPortfolioProject..CovidDeath
+--Where location like '%states%'
+order by 1,2
+
+
+-- Countries with Highest Infection Rate compared to Population
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From SQLPortfolioProject..CovidDeath
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- Countries with Highest Death Count per Population
+
+Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From SQLPortfolioProject..CovidDeath
+--Where location like '%states%'
+Where continent is not null 
+Group by Location
+order by TotalDeathCount desc
+
+
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From SQLPortfolioProject..CovidDeath
+--Where location like '%states%'
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+
+
+-- GLOBAL NUMBERS
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From SQLPortfolioProject..CovidDeath
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
